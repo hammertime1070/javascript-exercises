ae9250eaa1c1d806533e492f48935c3deab88022
hammertime1070 <mbkeith614@gmail.com>
2023-08-22 15:14:59 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    if (count < 0) return 'ERROR';
+    let newString = ''
+    for (let i = 0; i < count; i++) {
+        newString += string
+    }
+    return newString
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
