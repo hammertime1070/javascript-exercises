4e6416adf672a6220e4c8462f5f04784fa661900
hammertime1070 <mbkeith614@gmail.com>
2023-08-22 16:06:26 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    newArray = array.filter(checkArgs)
+    function checkArgs(element) {
+        return !args.includes(element)
+    }
+    return newArray
 };
 
 // Do not edit below this line
