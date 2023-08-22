c012c0dafffa32d2c4145a03a69cc7c41dbf99bc
hammertime1070 <mbkeith614@gmail.com>
2023-08-22 16:36:27 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(startValue, endValue) {
+    if (!Number.isInteger(startValue) || !Number.isInteger(endValue)) return 'ERROR'
+    if (startValue < 0 || endValue < 0) return 'ERROR'
+    let sum = 0;
+    let currentValue = Math.min(startValue, endValue);
+    while (currentValue <= Math.max(startValue, endValue)) {
+        sum += currentValue
+        currentValue++
+    }
+    return sum
+    
 };
 
 // Do not edit below this line
