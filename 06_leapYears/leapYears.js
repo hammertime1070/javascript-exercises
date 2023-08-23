fcd35940f7ea457da5b9754079949fd2e7996ea4
hammertime1070 <mbkeith614@gmail.com>
2023-08-23 08:54:43 -0400

    Complete leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    console.log(year % 4 === 0)
+    console.log(year % 100 === 0)
+    console.log(year % 400 === 0)
+    if ((year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0)) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
