6e9136c1f1421ead13706c663c0036359dcc4daf
hammertime1070 <mbkeith614@gmail.com>
2023-08-23 09:17:38 -0400

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  // Receive Fahrenheit
+  let celsiusTemp = (fahrenheitTemp - 32) * (5/9)
+  // Round to 1 Decimal
+  return Math.round(celsiusTemp * 10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  // Receive Celsius
+  let fahrenheitTemp = celsiusTemp * (9/5) + 32
+  // Round to 1 Decimal
+  return Math.round(fahrenheitTemp * 10)/10
 };
 
 // Do not edit below this line
