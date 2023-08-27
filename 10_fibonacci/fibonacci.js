28ce3bcfd1b4d95e3635b6ac22c5d29fe09fbb0e
hammertime1070 <mbkeith614@gmail.com>
2023-08-27 13:09:42 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return 'OOPS'
+    }
+    else {
+        sequence = [1, 1]
+        for (let i = 0; i < n - 2; i++) {
+            nextNumber = sequence[sequence.length - 1] + sequence[sequence.length - 2]
+            sequence.push(nextNumber)
+        }
+        return sequence[n - 1]
+    }
 };
 
 // Do not edit below this line
