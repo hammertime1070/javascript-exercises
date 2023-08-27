b39af6f6a8c37d5372f8a167972c2a37e9596702
hammertime1070 <mbkeith614@gmail.com>
2023-08-27 14:10:24 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    // My way looks way more complicated than the proposed solution
+    let currentYear = new Date().getFullYear()
+    ages = array.map((people) => {if (people.yearOfDeath){return people.yearOfDeath - people.yearOfBirth} else {return currentYear - people.yearOfBirth}})
+    return array[ages.indexOf(Math.max(...ages))]
 };
 
 // Do not edit below this line
