e82bf353f4fc42a028ed73d2047bb165ab55a5a3
hammertime1070 <mbkeith614@gmail.com>
2023-08-27 12:08:18 -0400

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(arg) {
+	let sum = 0;
+  for (let i = 0; i < arg.length; i++) {
+    sum += arg[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arg) {
+  let result = 1;
+  for (let i = 0; i < arg.length; i++) {
+    result = result * arg[i]
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let result = 1;
+  for (i = 0; i < y; i++) {
+    result = result * x
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	// If value is negative return ERROR
+  if (x < 0) {
+    return 'ERROR'
+  }
+  // if value is 0 return 1
+  else if (x == 0) {
+    return 1
+  }
+  else {
+    return (x * factorial(x - 1));
+  }
+
 };
 
 // Do not edit below this line
