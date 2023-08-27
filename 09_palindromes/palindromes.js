0a4fd32b14fee4cd566908cda9f1f0bd6c6a9a2b
hammertime1070 <mbkeith614@gmail.com>
2023-08-27 12:34:15 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (palindromeCandidate) {
+    processedPalindrome = palindromeCandidate.toLowerCase().replace(/[^a-z0-9]/gi, "")
+    return processedPalindrome == processedPalindrome.split("").reverse().join("")
 };
 
 // Do not edit below this line
